0e8a43217c441d5a0c19f8c7ac61f8dc25cf35e7
Hamish Lyall <hamishrlyall@gmail.com>
2018-05-28 14:11:36 +1000

    Phase 2 incomplete

--- Factorialiser/MainWindow.xaml.cs
+++ Factorialiser/MainWindow.xaml.cs
@@ -21,9 +21,20 @@ namespace Factorialiser
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
         public MainWindow()
         {
             InitializeComponent();
+            Loaded += OnLoad;
+            Closed += OnClosed;
+        }
+        private void OnLoad(object sender, RoutedEventArgs e)
+        {
+            logger.Trace("Main Window Loaded");
+        }
+        private void OnClosed(object sender, EventArgs e)
+        {
+            logger.Trace("Main Window Closed");
         }
 
         private void buttonCalculate_Click(object sender, RoutedEventArgs e)
@@ -32,18 +43,21 @@ namespace Factorialiser
             {
                 // check if textboxInput.Text is empty (or only contains white space), 
                 // if this is the case then throw a NullValueException
-
+                if (String.IsNullOrWhiteSpace(textBoxInput.Text)) throw new NullValueException();
                 //declare a variable here called input or datatype int
-
+                int input;
                 try
                 {
                     // try and parse the text input into textboxInput into an integer and assign it to input
                     // log a Debug level log event here with the message "MainForm.buttonCalculate_Click: input successfully parsed"
+                    input = int.Parse(textBoxInput.Text);
+                    logger.Debug("MainForm.buttonCalculate_Click: input successfully parsed");
                 }
                 catch
                 {
                     // log a Debug level log event here with the message "MainForm.buttonCalculate_Click: input parse failed"
                     // throw a NotIntegerException 
+                    logger.Debug("MainForm.buttonCalculate_Click: input parse failed");
                 }
 
 
@@ -52,6 +66,10 @@ namespace Factorialiser
 
                 // change the text of labelOutput to reflect
                 // log a Debug level log event here with the message "MainForm.buttonCalculate_Click: labelOutput successfully updated"
+                int factorial = Calculator.Factorial(input);
+                logger.Debug("MainForm.buttonCalculate_Click: Calculate.Factorial suceeded");
+                labelOutput.Content = factorial;
+                logger.Debug("MainForm.buttonCalculate_Click: labelOutput successfully updated");
 
 
             }
